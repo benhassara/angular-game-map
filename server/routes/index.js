b21d1d3d0e80cc1ee083380255a08f6a5563b236
benhassara <bhassara@gmail.com>
2015-10-05 16:46:35 -0600

    steam login works

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var steam = require('../auth/steam');
 
+router.get('/auth/steam',
+  steam.authenticate('steam'),
+  function(req, res) {
+});
 
+router.get('/auth/steam/return',
+  steam.authenticate(
+    'steam',
+    {failureRedirect: '/login'},
+    function(req, res) {
+      res.redirect('/');
+    })
+);
 module.exports = router;
