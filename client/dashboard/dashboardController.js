8c92d6f1864da22db7db9a27991ac4840d5f920e
Blackstock <charlieblackstock@gmail.com>
2015-10-08 16:51:38 -0600

    added fuzzyset to help match games to their game description from giant bomb

--- client/dashboard/dashboardController.js
+++ client/dashboard/dashboardController.js
@@ -10,7 +10,16 @@ function($scope, $http, $stateParams, mongoFactory, steamFactory) {
 
   steamFactory.getGames(steamid)
   .then(function(data) {
-    $scope.games = data.data.steam;
-    $scope.gb = data.data.gb;
+    $scope.games = data.data;
+    var steam = FuzzySet();
+    var steamGameNames = $scope.games.steam.map(function(game) {return game.name;});
+    steamGameNames.forEach(function(name) {steam.add(name);});
+
+    $scope.gbArray = [];
+    for (var i = 0; i < $scope.games.gb.length; i++) {
+      var query = steam.get($scope.games.gb[i].name);
+      var steamIndex = steamGameNames.indexOf(query[0][1]);
+      $scope.gbArray[steamIndex] = $scope.games.gb[i];
+    }
   });
 }]);
